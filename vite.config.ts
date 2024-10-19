51d0420e05b1d8993ef81a26ed42698a2f0a2f11
williamm56i <wutony791225@hotmail.com>
2024-10-19 15:15:58 +0800

    proxy for 5173 to 8080

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080/boai/', // 你的後端 API 服務地址
+        changeOrigin: true, // 避免後端 API 拒絕因為不同來源的請求
+      }
+    }
+  }
 })
